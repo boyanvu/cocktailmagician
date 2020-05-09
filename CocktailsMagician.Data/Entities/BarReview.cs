bd1e8cfc36ab0c765e4c4f6af0af69fc8606bb4e
Boyan Vuchev <bvuchev@gmail.com>
2020-05-09 23:14:06 +0300

    BarReview, User entities added

---
+++ CocktailsMagician.Data/Entities/BarReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailsMagician.Data.Entities
+{
+    public class BarReview
+    {
+        public int BarId { get; set; }
+        public Bar Bar { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime? DeletedOn { get; set; }
+    }
+}

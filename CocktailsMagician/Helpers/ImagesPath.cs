333f54ea0cd269148f61221bdd42d17eec14b8ae
Boyan Vuchev <bvuchev@gmail.com>
2020-06-07 15:17:50 +0300

    Bar details modification

---
+++ CocktailsMagician/Helpers/ImagesPath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CocktailsMagician.Helpers
+{
+    public static class ImagesPath
+    {
+        public static string ImgsPath { get => @"https://cocktails.blob.core.windows.net/bars/"; }
+    }
+}
